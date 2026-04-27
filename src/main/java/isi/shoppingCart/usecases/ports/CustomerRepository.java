3476f5d4c77b2b98cd92edc82db48fb75d7ac28e
da-sa-santos <da.sa.santos@unillanos.edu.co>
2026-04-27 11:20:03 -0500

    Delete src/main/java/isi/shoppingCart directory

--- src/main/java/isi/shoppingCart/usecases/ports/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package isi.shoppingCart.usecases.ports;
-
-import isi.shoppingCart.entities.Customer;
-
-public interface CustomerRepository {
-    Customer getCustomer();
-    void save(Customer customer);
-}
